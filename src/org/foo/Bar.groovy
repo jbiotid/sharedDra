aa350c349ae13d7d594b0d2b05f450d32cbdbed9
Jerome Biotidara <jbiotidara@gmail.com>
2018-04-19 10:44:23 -0400

    we more meat

--- src/org/foo/Bar.groovy
+++ src/org/foo/Bar.groovy
@@ -1,1 +1,9 @@
-println "Bar Hello world!"
+package org.foo
+class Utilities implements Serializable {
+  def steps
+  Utilities(steps) {this.steps = steps}
+  def mvn(args) {
+    println "Bar Hello world!"
+    steps.sh "${steps.tool 'Maven'}/bin/mvn -o ${args}"
+  }
+}
