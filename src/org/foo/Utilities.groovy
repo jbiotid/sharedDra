88659d2dc8b8dae3fe6ede8bb55ba40a23cedddb
Jerome Biotidara <jbiotidara@gmail.com>
2018-04-19 14:07:13 -0400

    test env

--- src/org/foo/Utilities.groovy
+++ src/org/foo/Utilities.groovy
@@ -18,7 +18,20 @@ package org.foo
 // }
 
 def checkOutFrom(repo) {
-  git url: "git@github.com:jbiotid/${repo}"
+  //git url: "git@github.com:jbiotid/${repo}"
+   withCredentials([string(credentialsId: 'BM_API_KEY', variable: 'bmApiKey')]) {
+      env.IBM_CLOUD_DEVOPS_API_KEY = "${bmApiKey}"
+      env.IBM_CLOUD_DEVOPS_ORG = 'UCCloud'
+      env.IBM_CLOUD_DEVOPS_APP_NAME = "${componentName}"
+      env.IBM_CLOUD_DEVOPS_TOOLCHAIN_ID = 'f63cd10b-2639-46a2-829c-89bc7188eb30'
+
+      publishBuildRecord gitBranch: "${gitBranch}", gitCommit: "${gitResult.GIT_COMMIT}", gitRepo: "${gitUrl}", result:"SUCCESS"
+
+      publishTestResult type:'unittest', fileLocation: 'test-results/unit/results.xml'
+      publishTestResult type:'fvt', fileLocation: 'test-results/func/results.xml', environment: 'DEV'
+
+      evaluateGate policy: 'Service Tests', forceDecision: 'false'
+    }
 }
 
 return this
