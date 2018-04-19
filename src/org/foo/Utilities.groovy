8a0de31198a8b657765347a1f0fefe37a11683c4
Jerome Biotidara <jbiotidara@gmail.com>
2018-04-19 13:47:14 -0400

    test other pipeline elements

--- src/org/foo/Utilities.groovy
+++ src/org/foo/Utilities.groovy
@@ -1,17 +1,24 @@
 package org.foo
-class Utilities implements Serializable {
-  def steps
-  Utilities(steps) {this.steps = steps}
-  def mvn(args) {
-    println "Bar Hello world!"
-    steps.sh "${steps.tool 'Maven'}/bin/mvn -o ${args}"
-  }
 
-  static builder(script) {
+// class Utilities implements Serializable {
+//   def steps
+//   Utilities(steps) {this.steps = steps}
+//   def mvn(args) {
+//     println "Bar Hello world!"
+//     steps.sh "${steps.tool 'Maven'}/bin/mvn -o ${args}"
+//   }
 
-      String filename = "/tmp/nastytest.txt"  
-// creates a new file test.txt  
-      boolean success = new File(filename).createNewFile() 
-        println "static Bar Hello world!"
-    }
+//   static builder(script) {
+
+//       String filename = "/tmp/nastytest.txt"  
+// // creates a new file test.txt  
+//       boolean success = new File(filename).createNewFile() 
+//         println "static Bar Hello world!"
+//     }
+// }
+
+def checkOutFrom(repo) {
+  git url: "git@github.com:jbiotid/${repo}"
 }
+
+return this
